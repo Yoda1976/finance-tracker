5b2918e71966f28103d6c1310769592006a83da8
Yoda1976 <t_braz02@hotmail.com>
2021-07-09 19:50:34 +0100

    Get stock value without AJAX

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,14 +1,13 @@
 class StocksController < ApplicationController
   def search
-    
     if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
       if @stock
-        @stock = Stock.new_lookup(params[:stock])
         render 'users/my_portfolio'
       else
         flash[:alert] = "Please enter a valid symbol to search"
         redirect_to my_portfolio_path
-      end
+      end    
     else
       flash[:alert] = "Please enter a symbol to search"
       redirect_to my_portfolio_path
