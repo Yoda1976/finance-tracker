7af7d5c18bc70ad2ee394e58c10b9bd0de7ebd6f
Yoda1976 <t_braz02@hotmail.com>
2021-07-09 17:45:57 +0100

    FUCK THIS

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,17 @@
+class StocksController < ApplicationController
+  def search
+    
+    if params[:stock].present?
+      if @stock
+        @stock = Stock.new_lookup(params[:stock])
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = "Please enter a valid symbol to search"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = "Please enter a symbol to search"
+      redirect_to my_portfolio_path
+    end
+  end
+end
